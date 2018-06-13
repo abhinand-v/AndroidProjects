cd8bab4000d7c6765745f294aabb52167285b1c8
Abhinand Valasseri <40170836+abhinand-v@users.noreply.github.com>
2018-06-13 22:24:53 +0530

    Update DbData.java

--- For-Your-Company/College Project Management/PMAdmin/app/src/main/java/av/pmadmin/DbData.java
+++ For-Your-Company/College Project Management/PMAdmin/app/src/main/java/av/pmadmin/DbData.java
@@ -1,7 +1,7 @@
 package av.pmadmin;
 
 public class DbData {
-    static final String serverUrl = "https://lgztecx.000webhostapp.com/project_man/";
+    static final String serverUrl = "https://your-server-url/"; // replace with your server URL
     static final String getTitle = serverUrl+"get_titles.php";
     static final String setStat = serverUrl+"set_status.php";
     static final String delete = serverUrl+"delete.php";
