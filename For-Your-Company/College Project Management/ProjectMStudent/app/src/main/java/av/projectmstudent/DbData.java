38fde10ca29a9a775697edb186c326e3c5f15abf
Abhinand Valasseri <40170836+abhinand-v@users.noreply.github.com>
2018-06-13 22:26:15 +0530

    Update DbData.java

--- For-Your-Company/College Project Management/ProjectMStudent/app/src/main/java/av/projectmstudent/DbData.java
+++ For-Your-Company/College Project Management/ProjectMStudent/app/src/main/java/av/projectmstudent/DbData.java
@@ -1,7 +1,7 @@
 package av.projectmstudent;
 
 public class DbData {
-    private static final String serverUrl = "https://lgztecx.000webhostapp.com/project_man/";
+    private static final String serverUrl = "https://your-server-url/"; // replace with your server URL
     static final String device_reg_read = serverUrl+"device_reg_read.php";
     static final String device_reg = serverUrl+"register_device.php";
     static final String submit_reg = serverUrl+"submit_reg.php";
